6f2e2dfaeaab0bfb949b34a741b0c0c4fd899233
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-30 17:56:50 +0530

    Create spam.py

---
+++ spam/modules/spam.py
@@ -0,0 +1,69 @@
+from pyrogram import filters
+from spam import bot
+from time import time
+from pyrogram.types import ChatPermissions
+from requests import get
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message
+
+def is_admin(group_id: int, user_id: int):
+    try:
+        user_data = bot.get_chat_member(group_id, user_id)
+        if user_data.status == 'administrator' or user_data.status == 'creator':
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+def call_back_filter(data):
+    return filters.create(lambda flt, _, query: flt.data in query.data,
+                          data=data)
+
+
+@bot.on_callback_query(call_back_filter("kick"))
+def kick_callback(_, query):
+    user = query.data.split(":")[2]
+    if is_admin(query.message.chat.id,
+                query.from_user.id) and query.data.split(":")[1] == "kick":
+        bot.ban_chat_member(query.message.chat.id, user)
+        bot.unban_chat_member(query.message.chat.id, user)
+        query.answer('Kicked!')
+        query.message.edit(
+            f'Kick User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
+            parse_mode='markdown')
+    else:
+        message.reply('You are not admin!')
+
+
+@bot.on_callback_query(call_back_filter("ban"))
+def ban_callback(_, query):
+    user = query.data.split(":")[2]
+    if is_admin(query.message.chat.id,
+                query.from_user.id) and query.data.split(":")[1] == "ban":
+        bot.ban_chat_member(query.message.chat.id, user)
+        query.answer('Banned')
+        query.message.edit(
+            f'Banned User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
+            parse_mode='markdown')
+    else:
+        message.reply('You are not admin!')
+
+
+@bot.on_callback_query(call_back_filter("mute"))
+def mute_callback(_, query):
+    user = query.data.split(":")[2]
+    if is_admin(query.from_user.id,
+                query.message.chat.id) and query.data.split(":")[1] == "mute":
+        bot.restrict_chat_member(
+            query.message.chat.id,
+            user,
+            ChatPermissions(can_send_messages=False),
+        )
+        query.answer('Muted!')
+        query.message.edit(
+            f'Muted User [{user}](tg://user?id={user})\n Admin User [{query.from_user.id}](tg://user?id={query.from_user.id})',
+            parse_mode='markdown')
+    else:
+        message.reply('You are not admin!')
