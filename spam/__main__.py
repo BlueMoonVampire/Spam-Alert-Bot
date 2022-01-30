5807a114ee6720ba15bb62e269354c775d5ad6b3
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-30 15:25:43 +0530

    Update __main__.py

--- spam/__main__.py
+++ spam/__main__.py
@@ -1,5 +1,5 @@
 from PyroBot import bot
-from PyroBot.modules import start
+from PyroBot.modules import start, vanitasalert, sibylalert, spamwatchalert
 from pyrogram import filters
 
 if __name__ == "__main__":
