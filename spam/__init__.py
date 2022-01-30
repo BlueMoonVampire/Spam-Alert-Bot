c2b1ce299e214e8d3aa7ac53068cc2649f526058
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-30 18:35:13 +0530

    Update __init__.py

--- spam/__init__.py
+++ spam/__init__.py
@@ -1,5 +1,10 @@
 import os
 from pyrogram import Client, filters
+from spam.db import DB
+
+db = DB(
+    "Mongo url"
+)
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
